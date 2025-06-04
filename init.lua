8d39e76d2c35db1586e4a6c929cb5de0412e71d3
Zac Hooper <zac@9epoch.ai>
2025-06-04 13:40:08 +1000

    feat: auto change dark mode, add jupyter notebook plugins

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
+-- Python Environment for Plugins
+vim.g.python3_host_prog = vim.fn.expand '/Users/zachooper/.config/python-neovim/.venv/bin/python3'
 
 -- [[ Setting options ]]
 --  For more options, you can see `:help option-list`
@@ -88,6 +90,8 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   require 'custom.plugins.dataform',
+  require 'custom.plugins.image',
+  require 'custom.plugins.molten',
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   { -- A`:help gitsigns` adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -605,17 +609,11 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'Shatur/neovim-ayu',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'ayu'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
   },
-
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -730,6 +728,23 @@ require('lazy').setup({
   },
 })
 
+-- Automatically switch colorscheme based on background mode
+local function set_colorscheme()
+  if vim.o.background == 'dark' then
+    vim.cmd.colorscheme 'ayu'
+  else
+    vim.cmd.colorscheme 'rose-pine-dawn'
+  end
+end
+
+-- Set colorscheme on startup
+set_colorscheme()
+
+-- Update colorscheme when background changes
+vim.api.nvim_create_autocmd('OptionSet', {
+  pattern = 'background',
+  callback = set_colorscheme,
+})
 -- My custom made plugins
 -- require 'custom.plugins.bigquery'
 
