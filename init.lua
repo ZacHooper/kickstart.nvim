c64a77dc9afa17c8be020993bc9a1ad5ec6ca7a1
Zac Hooper <zac.hooper@vinomofo.com>
2026-06-05 11:10:00 +1000

    chore: remove final traces of kickstart directory and rename augroups

--- init.lua
+++ init.lua
@@ -66,7 +66,7 @@ vim.keymap.set('n', '<C-S-d>', ':split<CR>', { desc = 'Split screen horizontally
 -- Highlight when yanking (copying) text. See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('zac-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
